eaa91d59d3211fc987a339dbc3cc5ce99298068f
chenhailiang <958304849@qq.com>
2018-05-28 09:11:03 +0800

    0528

--- src/components/pages/ShoppingMail.vue
+++ src/components/pages/ShoppingMail.vue
@@ -51,23 +51,11 @@
       </div>
     </div>
     <!-- floor -->
-    <div class="floor">
-      <div class="floor-anomaly">
-        <div class="floor-one"><img :src="floor1_0.image" width="100%"></div>
-        <div>
-          <div class="floor-two"><img :src="floor1_1.image" width="100%"></div>
-          <div><img :src="floor1_2.image" width="100%"></div>
-        </div>
-      </div>
-      <div class="floor-rule">
-        <div v-for="(item,index) in floor1.slice(3)" :key="index">
-          <img :src="item.image" width="100%">
-        </div>
-      </div>
-    </div>
+    <floor :floorData="floor1"></floor>
   </div>
 </template>
 <script>
+import floor from '@/components/component/floorComponent'
 import { swiper, swiperSlide } from 'vue-awesome-swiper'
 import 'swiper/dist/css/swiper.css'
 
@@ -94,7 +82,8 @@ export default {
   },
   components: {
     swiper,
-    swiperSlide
+    swiperSlide,
+    floor
   },
   created() {
     this.$http({
@@ -187,46 +176,6 @@ export default {
   border-right: 1px solid #eee;
   font-size: 12px;
   text-align: center;
-}
-
-.floor-anomaly {
-  display: flex;
-  flex-direction: row;
-  background-color: #fff;
-  border-bottom: 1px solid #ddd;
-}
-
-.floor-anomaly div {
-  width: 10rem;
-  -webkit-box-sizing: border-box;
-  -moz-box-sizing: border-box;
-  box-sizing: border-box;
-}
-
-.floor-one {
-  border-right: 1px solid #ddd;
-}
-
-.floor-two {
-  border-bottom: 1px solid #ddd;
-}
-
-.floor-rule {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  background-color: #fff;
-}
-
-.floor-rule div {
-  -webkit-box-sizing: border-box;
-  box-sizing: border-box;
-  width: 10rem;
-  border-bottom: 1px solid #ddd;
-}
-
-.floor-rule div:nth-child(odd) {
-  border-right: 1px solid #ddd;
 }
 
 </style>
